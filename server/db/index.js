6df38a7fc05c1121322d41d105ec4b644e49a5bd
Splix1 <86242483+Splix1@users.noreply.github.com>
2022-03-09 16:50:10 -0800

    added associations for users and scores

--- server/db/index.js
+++ server/db/index.js
@@ -1,5 +1,9 @@
 const db = require('./db');
 const User = require('./models/User');
+const Score = require('./models/Score');
+
+User.hasMany(Score);
+Score.belongsTo(User);
 
 module.exports = {
   db,
