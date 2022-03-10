7b640b421da2a68231bfe6b12b8eda78c6109033
hujeffy1 <hujeffy.1@gmail.com>
2022-03-10 15:14:33 -0500

    added Login/Logout to navbar

--- client/Routes.js
+++ client/Routes.js
@@ -1,17 +1,42 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './components/Home';
 import SingleRace from './components/SingleRace';
+import { me } from './redux';
+import Login from './components/Login';
+import Signup from './components/Signup';
 
 const Routes = () => {
+  const dispatch = useDispatch();
+
+  const isLoggedIn = useSelector((state) => {
+    return !!state.auth.id;
+  });
+
+  useEffect(() => {
+    dispatch(me());
+  }, []);
+
   return (
     <div>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/home" component={Home} />
-        <Route exact path="/singlesprint" component={SingleRace} />
-        <Route path="*" component={Home} />
-      </Switch>
+      {isLoggedIn ? (
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/home" component={Home} />
+          <Route exact path="/singlesprint" component={SingleRace} />
+          <Route path="*" component={Home} />
+        </Switch>
+      ) : (
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/home" component={Home} />
+          <Route exact path="/singlesprint" component={SingleRace} />
+          <Route path="/login" component={Login} />
+          <Route path="/signup" component={Signup} />
+          <Route path="*" component={Home} />
+        </Switch>
+      )}
     </div>
   );
 };
