8e61b6489534d92787335ffaaff4e42b23ce8518
hujeffy1 <hujeffy.1@gmail.com>
2022-03-09 20:29:22 -0500

    moved files around and added User thunks

--- client/store.js
+++
@@ -1,5 +0,0 @@
-import { createStore, applyMiddleware } from 'redux';
-import thunkMiddleware from 'redux-thunk';
-import appReducer from './redux';
-
-export default createStore(appReducer, applyMiddleware(thunkMiddleware));
