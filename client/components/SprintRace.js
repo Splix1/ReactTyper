b5049e6b1eb4d0b0779ca0329ce98fcb36af5a99
Splix1 <86242483+Splix1@users.noreply.github.com>
2022-03-13 11:50:38 -0700

    test

--- client/components/SprintRace.js
+++ client/components/SprintRace.js
@@ -29,6 +29,19 @@ function SprintRace() {
   let [countingDown, setCountingDown] = useState(false);
 
   useEffect(() => {
+    return () => {
+      if (raceCompleted === false) {
+        axios.delete('/api/scores/score', {
+          headers: {
+            userid: user.id,
+            raceid: raceId,
+          },
+        });
+      }
+    };
+  }, []);
+
+  useEffect(() => {
     let fetchingPlayers;
     if (raceId > 0 && racing === false && timer === 30) {
       fetchPlayers();
@@ -40,16 +53,6 @@ function SprintRace() {
         clearInterval(fetchingPlayers);
       }
     }
-    return async function cleanup() {
-      if (raceCompleted === false) {
-        await axios.delete('/api/scores/score', {
-          headers: {
-            userid: user.id,
-            raceid: raceId,
-          },
-        });
-      }
-    };
   }, [raceId, racing]);
 
   async function fetchPlayers() {
