b76835d4e27a5d34ded160a4f0386a9747ad6376
Splix1 <86242483+Splix1@users.noreply.github.com>
2023-02-24 08:22:03 -0800

    update db url and seed-and-start command

--- server/db/db.js
+++ server/db/db.js
@@ -21,7 +21,7 @@ if (process.env.DATABASE_URL) {
 }
 
 const db = new Sequelize(
-  process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
+  process.env.DB_URL || `postgres://localhost:5432/${databaseName}`,
   config
 );
 module.exports = db;
